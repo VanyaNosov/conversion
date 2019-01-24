f377e4ac8da19779ff3e270c6052e4811f021e6d
vanyanosov <nosov582125@gmail.com>
2019-01-24 17:53:16 +0200

    dark converter

--- conversion/currency_conversion.js
+++ conversion/currency_conversion.js
@@ -8,30 +8,195 @@ function arrayJson (json) {
         };
 };
 
-
-
 function conversion () {
     let number = document.getElementById("Converter").value;
     console.log(number)
-    let dollar_conversion = document.getElementById("convertion_dollar").value = innerHTML = parseInt(dollarPrice) * parseInt(number);
-    
-};
+    let dollar_conversion = document.getElementById("convertion_dollar").value = parseInt(dollarPrice) * parseInt(number);;
+  }
 
 
 function funс () {
     let loader = document.getElementById("page-preloader").style.display = 'none';
 };
 
+
 fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11')
     .then(res => res.json())
     .then(json => arrayJson(json), funс()) 
     .catch(error => {
-       alert(error.message)
+        funс()
+        alert(error.message)
     });  
 
 conversion();
 
 
+function addElement () {
+    let newDiv = document.createElement("div");
+        newDiv.innerHTML = "<h1>Money Converter</h1>"; 
+        my_div = document.getElementById("org_div1");
+        document.body.insertBefore(newDiv, my_div);
+        newDiv.style.cssText="color: red"; 
+}
+addElement()
+
+
+// let newDiv = document.createElement("div");
+//         newDiv.innerHTML = "<h1>Money Converter</h1>"; 
+//         my_div = document.getElementById("org_div1");
+//         document.body.insertBefore(newDiv, my_div);
+//         newDiv.style.cssText="color: red"; 
+// = innerHTML = parseInt(dollarPrice) * parseInt(number)
+// let dollarPrice = null;
+// function arrayJson (json) {
+//     console.log(json)
+//         for(var i = 0; i < json.length; i++){
+//             if(json[i].ccy === "USD"){
+//                 dollarPrice = json[i].buy;
+//             };
+//         };
+// };
+
+// let dollar_conversion = document.getElementById("convertion_dollar").value;
+
+// number.onchange = () => {
+//     dollar_conversion = parseInt(dollarPrice) * parseInt(number);
+//     console.log(dollar_conversion)
+//     console.log(number)
+//     console.log(dollarPrice)
+// }
+
+
+
+// }
+
+// function conversion () {
+//     let number = document.getElementById("Converter");
+//     console.log(number)
+//     let dollar_conversion = document.getElementById("convertion_dollar").value = parseInt(dollarPrice) * parseInt(number); 
+// };
+
+// let vanos = document.getElementById('bob').value;
+
+// vanos.onclick = function conversion_option () {
+    
+//     console.log(vanos);
+
+// function conversion () {
+
+   
+//     let number = document.getElementById("Converter").value;
+//     let dollar_conversion = document.getElementById("convertion_dollar").value;
+//     console.log(number)
+//     console.log(dollar_conversion)
+
+//     dollar_conversion.onchange = (dollarPrice) => {
+//         parseInt(dollarPrice) * parseInt(number);
+//         console.log(dollarPrice)
+      
+//       };
+    
+  
+// };
+
+// value = parseInt(dollarPrice) * parseInt(number)
+// = parseInt(dollarPrice) * parseInt(number)
+
+// function funс () {
+//     let loader = document.getElementById("page-preloader").style.display = 'none';
+// };
+
+// fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11')
+//     .then(res => res.json())
+//     .then(json => arrayJson(json), funс()) 
+//     .catch(error => {
+//         funс();
+//         alert(error.message);
+//     });  
+
+// document.body.onload = addElement;
+// let my_div = newDiv = null;
+
+// function addElement () {
+//     let newDiv = document.createElement("div");
+//         newDiv.innerHTML = "<h1>Money Converter</h1>"; 
+//         my_div = document.getElementById("org_div1");
+//         document.body.insertBefore(newDiv, my_div);
+//         newDiv.style.cssText="color: red"; 
+// }
+
+
+
+// let dollarPrice = null;
+// function arrayJson (json) {
+//     console.log(json)
+//         for(var i = 0; i < json.length; i++){
+//             if(json[i].ccy === "USD"){
+//                 dollarPrice = json[i].buy;
+//             };
+//         };
+// };
+
+
+
+// function conversion () {
+//     let number = document.getElementById("Converter");
+//     console.log(number)
+//     let dollar_conversion = document.getElementById("convertion_dollar").value = parseInt(dollarPrice) * parseInt(number); 
+// };
+
+
+// function funс () {
+//     let loader = document.getElementById("page-preloader").style.display = 'none';
+// };
+
+// fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11')
+//     .then(res => res.json())
+//     .then(json => arrayJson(json), funс()) 
+//     .catch(error => {
+//        alert(error.message)
+//     });  
+
+// conversion();
+
+
+
+ 
+
+// function arrayJson (json) {
+//     console.log(json)
+//         for(var i = 0; i < json.length; i++){
+//             if(json[i].ccy === "USD"){
+//                 dollarPrice = json[i].buy;
+//             };
+//         };
+// };
+
+
+
+// function conversion () {
+//     let number = document.getElementById("Converter").value;
+//     console.log(number)
+//     let dollar_conversion = document.getElementById("convertion_dollar").value = addEventListener("change", function() {
+//         innerHTML = parseInt(dollarPrice) * parseInt(number);
+//     });;
+//     console.log(dollar_conversion )
+    
+// };
+
+
+// function funс () {
+//     let loader = document.getElementById("page-preloader").style.display = 'none';
+// };
+
+// fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11')
+//     .then(res => res.json())
+//     .then(json => arrayJson(json), funс()) 
+//     .catch(error => {
+//        alert(error.message)
+//     });  
+
+// conversion();
 
 
 
@@ -42,6 +207,8 @@ conversion();
 
 
 
+
+// .value = innerHTML = parseInt(dollarPrice) * parseInt(number);
 
 
 // .value = innerHTML = parseInt(dollarPrice) * parseInt(number);
@@ -124,6 +291,10 @@ conversion();
 //     return dollarPrice;
 // };
 
+// function funс () {
+//     let loader = document.getElementById("page-preloader").style.display = 'none';
+// };
+// funс();
 
 // async function conversion (dollarPrice) { 
 //     const result = await functionSome();
